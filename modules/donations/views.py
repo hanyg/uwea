4e24e4507d2f6ae78a277e0edfd5b5a3f89216c9
Hany Greiss <hany_gg@yahoo.ca>
2017-03-17 13:23:12 +0000

    Basic ORM integrated

--- modules/donations/views.py
+++ modules/donations/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Envelope
 
 def donations(request):
-    return render(request, 'donations/donations.html', {})
+    envelopes = Envelope.objects.order_by('created_on')
+    return render(request, 'donations/donations.html', { 'envelopes': envelopes })
